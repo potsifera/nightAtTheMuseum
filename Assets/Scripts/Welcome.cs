c622d005dc3656e2c8f5a47ec103ceafdfbb162b
Grace M <ps28@ps28.local>
2018-02-27 09:40:57 -0600

    welcome canvas disappears after selecting start

---
+++ Assets/Scripts/Welcome.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Welcome : MonoBehaviour {
+
+	public void Start(){
+		gameObject.GetComponent<Renderer>().enabled = true;
+	}
+}
