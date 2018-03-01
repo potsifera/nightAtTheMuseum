61ed81d28396d5c554c024b3ff10c72f1694fb92
Grace <grace@gracemontoya.com>
2018-03-01 13:01:12 -0600

    bug fix for waypoint distances
    modify the range of the raycaster as well as the its debug mode display by altering the following lines in the GvrReticlePointer.cs script // Minimum distance of the reticle (in meters).
      public const float RETICLE_DISTANCE_MIN = 2.45f;
      // Maximum distance of the reticle (in meters).
      public const float RETICLE_DISTANCE_MAX = 40.0f;

--- Assets/GoogleVR/Scripts/UI/GvrReticlePointer.cs
+++ Assets/GoogleVR/Scripts/UI/GvrReticlePointer.cs
@@ -28,9 +28,9 @@ public class GvrReticlePointer : GvrBasePointer {
   public const float RETICLE_GROWTH_ANGLE = 1.5f;
 
   // Minimum distance of the reticle (in meters).
-  public const float RETICLE_DISTANCE_MIN = 0.45f;
+  public const float RETICLE_DISTANCE_MIN = 2.45f;
   // Maximum distance of the reticle (in meters).
-  public const float RETICLE_DISTANCE_MAX = 10.0f;
+  public const float RETICLE_DISTANCE_MAX = 40.0f;
 
   /// Number of segments making the reticle circle.
   public int reticleSegments = 20;
