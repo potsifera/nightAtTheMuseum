152504e41ea4f2df39ab9447d4f327613481f928
Grace <grace@gracemontoya.com>
2018-03-04 14:04:13 -0600

    Adds video controllers
    Adds first video controller to project

---
+++ Assets/Scripts/PlayVideos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideos : MonoBehaviour {
+
+	public GameObject movie;
+
+
+	public void PlayFilm() {
+
+		
+	}
+}
---
+++ Assets/WorldSpaceVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class WorldSpaceVideo : MonoBehaviour {
+
+	public Material playButtonMaterial;
+	public Material pauseButtonMaterial;
+	public Renderer buttonRenderer;
+
+	private VideoPlayer videoPlayer;
+
+	private void Awake() {
+		videoPlayer = GetComponent<VideoPlayer>();
+
+	}
+
+	public void PlayPause() {
+		if (videoPlayer.isPlaying) {
+			videoPlayer.Pause();
+			buttonRenderer.material = playButtonMaterial;
+		} else {
+			videoPlayer.Play();
+			buttonRenderer.material = pauseButtonMaterial;
+		}
+	}
+
+
+}
